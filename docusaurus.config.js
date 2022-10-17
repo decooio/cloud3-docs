c2781d33e7c87f830f5f045005ed661a5c6cf2ba
gavfu <gavfu@outlook.com>
2022-10-17 10:49:48 +0800

    Basic site setup

---
+++ docusaurus.config.js
@@ -0,0 +1,53 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Cloud3 Docs',
+  tagline: 'Store in IPFS W3Bucket, Decentralized, Guaranteed & Alive.',
+  url: 'https://docs.cloud3.cc',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.svg',
+  organizationName: 'decooio', // Usually your GitHub org/user name.
+  projectName: 'cloud3-docs', // Usually your repo name.
+  themeConfig: {
+    prism: {
+      additionalLanguages: ['java'],
+    },
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Cloud3 Logo',
+        src: 'img/logo.svg',
+      },
+    },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    }
+
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+
+          // editUrl: 'https://github.com/decooio/cloud3-docs/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/decooio/cloud3-docs/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
